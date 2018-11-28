8c337e6295cb21e8e516e348fafb552573d26a4c
Xhq <xuhaoqing2013@hotmail.com>
2018-11-28 23:16:22 +0800

    D10 Problem Ver0.0.1

---
+++ java/source/xhq/D10/D10Calculator.java
@@ -0,0 +1,95 @@
+/*
+ * 解决杰哥的D10四则运算结果尾数概率问题
+ */
+package xhq.D10;
+
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class D10Calculator {
+	public final static DecimalFormat FORMAT = new DecimalFormat("0.00%");
+	private static ArrayList<Character> modes_ = new ArrayList<Character>();
+	private static int numOfD10_;
+	private static D10Frame frame = new D10Frame();
+	private static int[] results = new int[11];
+	private static int cursor;
+	private static int total = 0;
+	
+	private static int next_(int pre, int next) {
+		switch (modes_.get(cursor)) {
+		case '+':
+			return pre + next;
+		case '-':
+			return pre - next;
+		case '*':
+			return pre * next;
+		case '/':
+			return pre / next;
+		default:
+			return 1000000;
+		}
+	}
+	public static void calc(int c) {
+		cursor = 0;
+		int temp = c % 10;
+		if (c % 10 == 0)
+			temp = 10;
+		c /= 10;
+		for (int i = 1; i < numOfD10_; i++) {
+			if (c % 10 == 0)
+				temp = next_(temp, 10);
+			else
+				temp = next_(temp, c % 10);
+			c /= 10;
+//			if (temp == 1000000) {
+//				results[10]++;
+//				total++;
+//				return;
+//			}
+			cursor++;
+		}
+		total++;
+		results[Math.abs(temp) % 10]++;
+	}
+	
+	public static void main(String[] args) {
+		frame.setVisible(true);
+	}
+	
+	public static void go() {
+		frame.textArea.setText("");
+		total = 0;
+		for (int i = 0; i <= 10; i++)
+			results[i] = 0;
+		modes_.clear();
+		for (int i = 0; i < 5; i++) {
+			switch (frame.lists[i].getSelectedIndex()) {
+			case 0:
+				modes_.add('+');
+				break;
+			case 1:
+				modes_.add('-');
+				break;
+			case 2:
+				modes_.add('*');
+				break;
+			case 3:
+				modes_.add('/');
+				break;
+			case 4: case -1:
+				i = 5;
+				break;
+			}
+		}
+		numOfD10_ = modes_.size() + 1;
+		for (int i = 0; i < Math.pow(10, numOfD10_); i++) {
+			calc(i);
+		}
+		frame.textArea.append("Total: " + total + "\r\n");
+		for (int i = 0; i < 10; i++) {
+			frame.textArea.append(i + ": " + results[i] + "\t" + FORMAT.format(((double)results[i] / total)) + "\r\n");
+		}
+		frame.textArea.append("/0: " + results[10] + "\t" + FORMAT.format(((double)results[10] / total)));
+	}
+
+}
---
+++ java/source/xhq/D10/D10Frame.java
@@ -0,0 +1,155 @@
+package xhq.D10;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ListSelectionModel;
+import javax.swing.JTextArea;
+
+public class D10Frame extends JFrame {
+
+	private JPanel contentPane;
+	JList list1;
+	JList list2;
+	JList list3;
+	JList list4;
+	JList[] lists = new JList[4];
+	JTextArea textArea;
+	/**
+	 * Launch the application.
+	 *
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					D10Frame frame = new D10Frame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+*/
+	/**
+	 * Create the frame.
+	 */
+	public D10Frame() {
+		setResizable(false);
+		setTitle("D10 Calculator");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 446, 433);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.NORTH);
+		
+		JLabel label = new JLabel("{ [ ( D10 ");
+		panel_1.add(label);
+		
+		list1 = new JList();
+		list1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list1.setModel(new AbstractListModel() {
+			String[] values = new String[] {"+", "-", "*", "/", "None"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list1.setVisibleRowCount(5);
+		list1.setValueIsAdjusting(true);
+		panel_1.add(list1);
+		
+		JLabel lblD = new JLabel(" D10 ) ");
+		panel_1.add(lblD);
+		
+		list2 = new JList();
+		list2.setModel(new AbstractListModel() {
+			String[] values = new String[] {"+", "-", "*", "/", "None"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list2.setVisibleRowCount(5);
+		list2.setValueIsAdjusting(true);
+		panel_1.add(list2);
+		
+		JLabel lblD_1 = new JLabel(" D10 ] ");
+		panel_1.add(lblD_1);
+		
+		list3 = new JList();
+		list3.setModel(new AbstractListModel() {
+			String[] values = new String[] {"+", "-", "*", "/", "None"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list3.setVisibleRowCount(5);
+		list3.setValueIsAdjusting(true);
+		panel_1.add(list3);
+		
+		JLabel lblD_2 = new JLabel(" D10 } ");
+		panel_1.add(lblD_2);
+		
+		list4 = new JList();
+		list4.setModel(new AbstractListModel() {
+			String[] values = new String[] {"+", "-", "*", "/", "None"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		panel_1.add(list4);
+		
+		JLabel lblD_3 = new JLabel(" D10");
+		panel_1.add(lblD_3);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.SOUTH);
+		
+		JButton btnCalculate = new JButton("Calculate");
+		btnCalculate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				D10Calculator.go();
+			}
+		});
+		panel.add(btnCalculate);
+		
+		JLabel lblResult = new JLabel("Result:");
+		contentPane.add(lblResult, BorderLayout.WEST);
+		
+		lists[0] = list1;
+		lists[1] = list2;
+		lists[2] = list3;
+		lists[3] = list4;
+		
+		textArea = new JTextArea();
+		contentPane.add(textArea, BorderLayout.CENTER);
+		textArea.setPreferredSize(new Dimension(300,200));
+	}
+
+}
